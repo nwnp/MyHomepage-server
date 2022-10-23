7744198c631fa62822d185b637a3eecaa3be049a
nwnp <jjw_0418@naver.com>
2022-10-23 19:51:28 +0900

    feat: logger middleware setup

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { LoggerMiddle } from 'src/middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,4 +8,8 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddle).forRoutes('*');
+  }
+}
