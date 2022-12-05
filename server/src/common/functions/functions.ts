19565a74c454e21d5866209ddc11f7e12a732957
nwnp <jjw_0418@naver.com>
2022-12-05 16:37:52 +0900

    fix: parsing date(createdAt) success

--- server/src/common/functions/functions.ts
+++ server/src/common/functions/functions.ts
@@ -1,4 +1,5 @@
 import { Calendar } from '../databases/calendars.entity';
+import { Post } from '../databases/posts.entity';
 
 export const map = (cb, iter: string[] | Calendar[]): string[] => {
   const result = [];
@@ -10,17 +11,21 @@ export const map = (cb, iter: string[] | Calendar[]): string[] => {
 
 export const returnDate = async (data: Calendar[]): Promise<Calendar[]> => {
   const parsingData = map(
-    (p: Date) => p.toISOString().replace(/T/, ' ').split('.')[0],
+    (p: Date) => p.toISOString().replace(/T/, ' '),
     map(
       (p: Calendar) => p.createdAt,
       data.filter((p) => p.createdAt),
     ),
   );
   const result = [];
+
   for (let i = 0; i < data.length; i++) {
+    const tempDate = new Date(parsingData[i]);
     result.push({
       ...data[i],
-      createdAt: parsingData[i],
+      createdAt: new Date(tempDate.setHours(tempDate.getHours() + 9))
+        .toISOString()
+        .replace(/T/, ' '),
     });
   }
   return result;
