4eacbfee23bf0e1a79972d27425cc617470f1d1d
nwnp <jjw_0418@naver.com>
2022-12-21 11:17:31 +0900

    feat: app.module 수정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -86,13 +86,15 @@ import slackConfig from 'src/config/slack.config';
       driver: ApolloDriver,
       debug: false,
       cache: 'bounded',
-      playground: process.env.NODE_ENV === 'production' ? false : true,
+      // playground: process.env.NODE_ENV === 'production' ? false : true,
+      playground: false,
       typePaths: ['./**/*.graphql'],
       cors: {
         origin: [
-          process.env.NODE_ENV === 'production'
-            ? 'https://my-page.co.kr'
-            : 'http://localhost:8081',
+          'https://my-page.co.kr',
+          // process.env.NODE_ENV === 'production'
+          //   ? 'https://my-page.co.kr'
+          //   : 'http://localhost:8081',
         ],
         credentials: true,
       },
