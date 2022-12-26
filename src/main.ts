0a156e692602c3ebe70837a0742a83e60075e871
nwnp <jjw_0418@naver.com>
2022-12-27 02:12:17 +0900

    feat: helmet module install & setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function start() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function start() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  if (process.env.NODE_ENV === 'production') app.use(helmet());
 
   await app.listen(PORT, () => {
     const logger = new Logger('MAIN');
